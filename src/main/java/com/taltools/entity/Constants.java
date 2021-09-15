5198304942cb19ecd95e7ba12396b81e12b37246
CuiZhongyuan <365450906@qq.com>
2021-09-15 21:10:15 +0800

    定义需要发送的实体

---
+++ src/main/java/com/taltools/entity/Constants.java
@@ -0,0 +1,118 @@
+package com.taltools.entity;
+
+/**
+ * @author zhangjunyong
+ * @description 通用常量
+ * @date 2021-03-16 14:32
+ */
+public class Constants {
+
+    /**
+     * 逗号分隔符
+     */
+    public static final String SEPARATE_COMMA = ",";
+
+    /**
+     * 双引号空
+     */
+    public static final String SEPARATE = "";
+
+    /**
+     * 空格
+     */
+    public static final String SEPARATE_SPACE = " ";
+
+    /**
+     * 横杠
+     */
+    public static final String SEPARATE_HORIZONTAL = "-";
+
+    /**
+     * 竖杠
+     */
+    public static final String SEPARATE_VERTICAL = "|";
+
+    /**
+     * 分号
+     */
+    public static final String SEPARATE_SEMICOLON = ";";
+
+    /**
+     * 点号
+     */
+    public static final String SEPARATE_SPOT = ".";
+
+    /**
+     * 顿号
+     */
+    public static final String STOP_SIGN = "、";
+
+
+
+    /**
+     * 目录分隔符
+     */
+    public static final String CATALOG_SPOT = ">";
+
+    /**
+     * 默认页码
+     */
+    public static final Integer DEFAULT_PAGE_INDEX = 1;
+
+    /**
+     * 默认每页大小
+     */
+    public static final Integer DEFAULT_PAGE_SIZE = 10;
+
+    /**
+     * 默认每页大小
+     */
+    public static final Integer DEFAULT_TOTAL_COUNT = 10;
+
+
+    /**
+     * 内容分类,全选code
+     */
+    public static final String ALL_LEVEL_CODE = "0000";
+    /**
+     * 内容分类,全选中文名
+     */
+    public static final String ALL_LEVEL_NAME = "全部";
+
+    /**
+     * 内容分类,分校类型
+     */
+    public static final String CITY = "city";
+    /**
+     * 内容分类,年级类型
+     */
+    public static final String GRADE = "grade";
+    /**
+     * 内容分类,学科类型
+     */
+    public static final String SUBJECT = "subject";
+    /**
+     * 内容分类,学期类型
+     */
+    public static final String TERM = "term";
+    /**
+     * 内容分类,班级年份类型
+     */
+    public static final String YEAR = "year";
+
+    /**
+     * 0
+     */
+    public static final Integer ZERO = 0;
+
+    /**
+     * 1
+     */
+    public static final Integer ONE = 1;
+
+    /**
+     * 系统
+     */
+    public static final String SYSTEM = "system";
+
+}
