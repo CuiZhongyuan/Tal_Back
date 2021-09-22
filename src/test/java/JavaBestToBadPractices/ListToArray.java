c3190c0f905f30b4399c2e361efec9f0c8a7b312
CuiZhongyuan <365450906@qq.com>
2021-09-22 20:47:18 +0800

    集合转数组：toArray()优化写法

---
+++ src/test/java/JavaBestToBadPractices/ListToArray.java
@@ -0,0 +1,57 @@
+package JavaBestToBadPractices;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  集合转数组：toArray()
+ *  有些场景下（比如入参要求）需要将集合（比如List）转为数组类型，利用集合的toArray方法应该最为方便的，
+ *  对于toArray()无参方法其返回的是Object[]，强制转其他类型数组会ClassCastException。
+ *  推荐使用带参数的toArray(T[])，不过使用上需要注意
+ */
+public class ListToArray {
+    @Test
+    //list转数组
+    public void arrayError(){
+        List<String> listToArray = new ArrayList<>();
+        listToArray.add("jav");
+        listToArray.add(0, "python");
+        listToArray.add("php");
+        listToArray.add("c#");
+        String[] str = (String[]) listToArray.toArray();
+        System.out.println(str);
+    }
+
+    @Test
+    //list转数组
+    public void arrayObject(){
+        List<String> listToArray = new ArrayList<>();
+        listToArray.add("jav");
+        listToArray.add(0, "python");
+        listToArray.add("php");
+        listToArray.add("c#");
+        String[] s = new String[listToArray.size()];
+        Object[] o = listToArray.toArray();
+        for (int i = 0; i < o.length; i++) {
+            s[i] = o[i].toString();
+        }
+        System.out.println(Arrays.toString(s));
+    }
+
+    @Test
+    //list转数组-优化
+    public void OptimizeCode(){
+        List<String> listToArray = new ArrayList<>();
+        listToArray.add("jav");
+        listToArray.add(0, "python");
+        listToArray.add("php");
+        listToArray.add("c#");
+        String[] str = listToArray.toArray(new String[0]);
+        System.out.println(Arrays.toString(str));
+    }
+
+
+}
