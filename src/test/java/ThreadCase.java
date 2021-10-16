d74b4bcf6bac5ebeeb2583817bf8f22baebd91fd
CuiZhongyuan <365450906@qq.com>
2021-10-16 16:55:00 +0800

    多线程并发请求

--- src/test/java/ThreadCase.java
+++ src/test/java/ThreadCase.java
@@ -1,23 +1,25 @@
+import com.taltools.utils.RestTemplateUtils;
+
 public class ThreadCase {
-    private static final String TEXT="测试多线程，学习多线程"+"。。。。。。";
+    private static final String URL="http://127.0.0.1:8080/api/getPage?pageNum=1&pageSize=10";
     /**
      * 多线程学习
      */
     public static void main(String[] args){
         System.out.println("程序开始，执行的线程名叫："+Thread.currentThread().getName());
         //需要启动几个线程执行程序
-        for (int i=0;i<=3;i++){
-            Thread thread = new Thread(new PrintText(TEXT,200),"我的线程--"+i);
+        for (int i=0;i<3;i++){
+            Thread thread = new Thread(new PrintText(URL,200),"当前的线程名称--"+i);
             thread.start();
         }
         System.out.println("启动的线程结束，名字叫做"+Thread.currentThread().getName());
 
     }
     static class PrintText implements Runnable{
-        private String text;
+        private String url;
         private long time;
-        public PrintText(String text,long time){
-            this.text=text;
+        public PrintText(String url,long time){
+            this.url=url;
             this.time=time;
         }
 
@@ -25,7 +27,7 @@ public class ThreadCase {
         public void run() {
             try {
                 System.out.println("执行这段代码的名字叫做"+Thread.currentThread().getName());
-                printRseult(text,time);
+                printRseult(url,time);
                 System.out.println(Thread.currentThread().getName()+"执行结果");
             }catch (Exception e){
                 e.printStackTrace();
@@ -33,12 +35,12 @@ public class ThreadCase {
 
         }
 
-        private void printRseult(String text, long time) throws InterruptedException {
-            for (char c:text.toCharArray()){
-                Thread.sleep(time);
-                System.out.print(c);
-            }
+        private void printRseult(String url, long time) throws InterruptedException {
+            Thread.sleep(time);
+            System.out.println("---------http并发请求--------------");
+            System.out.println(RestTemplateUtils.get(url,String.class).getBody());
         }
+
 
 
     }
