cf45433a7f6787c2dff60de8128053e1bffa8ed4
CuiZhongyuan <365450906@qq.com>
2021-10-22 14:02:26 +0800

    异步同步测试

--- src/test/java/Asynchronous.java
+++ src/test/java/Asynchronous.java
@@ -8,9 +8,6 @@ public class Asynchronous {
      * 异步操作
      * 打印1-n，如果数字是n/2,那么久插入一个为期2s的任务
      */
-    public static int count;
-    public static int limit;
-    public static Object moniter = new Object();
     public static void main(String[] args){
 
         Scanner scanner= new Scanner(System.in);
