fef1714a403d994efd8dbec7dfa0c794b8d3e0cb
CuiZhongyuan <365450906@qq.com>
2021-10-28 09:39:00 +0800

    多线程测试示例

--- src/test/java/ThreadLocalCase.java
+++ src/test/java/ThreadLocalCase.java
@@ -19,13 +19,12 @@ public class ThreadLocalCase {
         public runThreadLocal(String url){
             this.url=url;
         }
-
         @Override
         public void run() {
             try{
                 //线程内并发数，当前线程内并发俩个
                 for (int i=0;i<1;i++){
-                    //ThreadLocal.get方法获取线程变量
+                    //发送http请求获取响应结果
                     String getBody = RestTemplateUtils.get(url,String.class).getBody();
                     threadLocal.set(getBody);
                     System.out.println("当前线程名："+Thread.currentThread().getName()+"当前线程第几次并发数："+i+"---:"+DateUtils.getSystime()+": "+threadLocal.get());
