66ae1ded98ae526a225b2029d4e860d2c6f27e69
CuiZhongyuan <365450906@qq.com>
2021-11-21 21:36:39 +0800

    groovy测试脚本

--- src/test/java/groovytest/GroovyTest.groovy
+++ src/test/java/groovytest/GroovyTest.groovy
@@ -2,10 +2,8 @@ package groovytest
 
 class GroovyTest {
    static void main(String[] args){
-       def name = 'java'
-       def greeting = "Hello ${name}"
-       def  myVar = greeting;
-       println(myVar)
+       def colors = [jave: '学习', python: '不学习', c: '底层']
+      println(colors.jave)
    }
     static String testGroovy(){
         def name = 'java'
